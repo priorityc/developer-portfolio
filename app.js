3e1d88cd276136a514316935815e953025793e3b
priorityc <petya83dimitrova@gmail.com>
2022-09-28 12:35:58 +0100

    Validating and submiting the form

--- app.js
+++ app.js
@@ -27,33 +27,51 @@
 //   thankyou.innerText = "Thanks your message was send";
 // };
 // document.querySelector(".send-btn").addEventListener("click", validateForm);
+
 // the Hiden Modal
 const modal = document.querySelector(".modal");
-
 const overlayBlur = document.querySelector(".overlay");
 const hireMe = document.querySelector(".hero-text-button");
-
+const contactFormData = {};
 const closeM = document.querySelector(".close-modal");
+const validateB = document.querySelector(".validate-form");
+const tnkyou = document.getElementById("thanks");
 
+// open the MODAL
 const openModal = function () {
   modal.classList.remove("hidden");
   overlayBlur.classList.remove("hidden");
 };
 hireMe.addEventListener("click", openModal);
 
+// validate the form and Submit it
+validateContactForm = () => {
+  contactFormData.Name = document.forms["form1"]["first_name"].value;
+  contactFormData.LastName = document.forms["form1"]["second_name"].value;
+  contactFormData.Message = document.getElementById("contact-message").value;
+  contactFormData.Email = document.getElementById("contact-email").value;
+
+  if (!contactFormData.Name) {
+    alert("Name must be filled out");
+    return false;
+  } else if (!contactFormData.LastName) {
+    alert("Last Name must be filled out");
+    return false;
+  } else if (contactFormData.Email.indexOf("@") == -1) {
+    alert("Email must be filled out");
+    return false;
+  } else if (!contactFormData.Message) {
+    alert("Message must be filled out");
+    return false;
+  } else {
+    tnkyou.textContent = "Thanks your message have been submited!";
+  }
+};
+
+// close the Modal
 const closeModal = function () {
   modal.classList.add("hidden");
   overlayBlur.classList.add("hidden");
 };
 closeM.addEventListener("click", closeModal);
 overlayBlur.addEventListener("click", closeModal);
-// Set up a thank you message
-const btnSubmit = document.querySelector(".hero-text-btn");
-const tnkyou = document.getElementById("thanks");
-console.log(tnkyou);
-
-const sayThanks = function () {
-  console.log(`Thanks`);
-  tnkyou.textContent = `Thank you your message have been send!`;
-};
-btnSubmit.addEventListener("click", sayThanks);
