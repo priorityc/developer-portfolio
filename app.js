55b55d6526b69b6606f0d72a537c3b1c0dd7dd8c
priorityc <petya83dimitrova@gmail.com>
2022-11-13 01:44:01 +0000

    Contact page

--- app.js
+++ app.js
@@ -87,93 +87,114 @@ const closeModal = function () {
 closeM.addEventListener("click", closeModal);
 overlayBlur.addEventListener("click", closeModal);
 
+// Validate and submit the other form
+const footerForm = {};
+const thansMsg = document.getElementById("tnk");
+validateFooterForm = () => {
+  footerForm.text = document.forms["myform"]["text"].value;
+  footerForm.email = document.forms["myform"]["email"].value;
+  footerForm.message = document.getElementById("message").value;
+
+  if (!footerForm.text) {
+    alert("Name must be filled out");
+    return false;
+  } else if (!footerForm.email.indexOf("@") == -1) {
+    alert("Email must be filled out");
+    return false;
+  } else if (!footerForm.message) {
+    alert("Message must be filled out");
+    return false;
+  } else {
+    thansMsg.textContent = "Thanks your message was send!";
+  }
+};
 // the about me facts
-const rightBTN = document.querySelector(".right");
-const leftBTN = document.querySelector(".left");
-const hidendiv = document.querySelector(".text-left_one");
-const hidendiv2 = document.querySelector(".text-left_two");
-const textP = document.querySelector(".text-left");
-const hidendiv3 = document.querySelector(".text-left_three");
-let slideimages = new Array();
-let text = new ArrayBuffer();
+// const rightBTN = document.querySelector(".right");
+// const leftBTN = document.querySelector(".left");
+// const hidendiv = document.querySelector(".text-left_one");
+// const hidendiv2 = document.querySelector(".text-left_two");
+// const textP = document.querySelector(".text-left");
+// const hidendiv3 = document.querySelector(".text-left_three");
+// let slideimages = new Array();
+// let text = new ArrayBuffer();
 
-slideimages[0] = new Image();
-slideimages[0].src = "images-slide/house.jpg";
+// slideimages[0] = new Image();
+// slideimages[0].src = "images-slide/house.jpg";
 
-slideimages[1] = new Image();
-slideimages[1].src = "images-slide/house.jpg";
+// slideimages[1] = new Image();
+// slideimages[1].src = "images-slide/house.jpg";
 
-slideimages[2] = new Image();
-slideimages[2].src = "images-slide/new-career.jpg";
-text[0] = "";
+// slideimages[2] = new Image();
+// slideimages[2].src = "images-slide/new-career.jpg";
+// text[0] = "";
 
-let step = 0;
+// let step = 0;
 
-slide = () => {
-  document.querySelector(".warehouse").src = slideimages[step].src; //slideimages[0].src
+// slide = () => {
+//   document.querySelector(".warehouse").src = slideimages[step].src; //slideimages[0].src
 
-  if (step < 2) {
-    step++;
-  } else {
-    step = 0;
-  }
-  //hide text one and three and show text 2
-  hidendiv.classList.add("hidden-p"); //hide div
-  hidendiv3.classList.add("hidden-p");
-  // hide1
-  hidendiv3.classList.add("hidden-p");
-  hidendiv2.classList.remove("hidden-p"); //show 2
-  //hide 3
-  //show text 3 but hide two
-  hidendiv2.classList.add("hidden-p");
-  hidendiv3.classList.remove("hidden-p");
-};
-//hide 2
+//   if (step < 2) {
+//     step++;
+//   } else {
+//     step = 0;
+//   }
+//   //hide text one and three and show text 2
+//   hidendiv.classList.add("hidden-p"); //hide div
+//   hidendiv3.classList.add("hidden-p");
+//   // hide1
+//   hidendiv3.classList.add("hidden-p");
+//   hidendiv2.classList.remove("hidden-p"); //show 2
+//   //hide 3
+//   //show text 3 but hide two
+//   hidendiv2.classList.add("hidden-p");
+//   hidendiv3.classList.remove("hidden-p");
+// };
+// //hide 2
 
-rightBTN.addEventListener("click", slide);
+// rightBTN.addEventListener("click", slide);
 
-backslide = () => {
-  document.querySelector(".warehouse").src = slideimages[step].src;
-  if (step > 2) {
-    step--;
-  } else {
-    step = 0;
-  }
-  hidendiv.classList.add("hidden-p"); // hide1
-  hidendiv2.classList.remove("hidden-p"); //show 2
+// backslide = () => {
+//   document.querySelector(".warehouse").src = slideimages[step].src;
+//   if (step > 2) {
+//     step--;
+//   } else {
+//     step = 0;
+//   }
+//   hidendiv.classList.add("hidden-p"); // hide1
+//   hidendiv2.classList.remove("hidden-p"); //show 2
 
-  hidendiv3.classList.add("hidden-p"); //hide 3
-  hidendiv.classList.remove("hidden-p"); //show 1
-  hidendiv2.classList.add("hidden-p"); // 2
+//   hidendiv3.classList.add("hidden-p"); //hide 3
+//   hidendiv.classList.remove("hidden-p"); //show 1
+//   hidendiv2.classList.add("hidden-p"); // 2
 
-  hidendiv.classList.remove("hidden-p");
-};
-leftBTN.addEventListener("click", backslide);
+//   hidendiv.classList.remove("hidden-p");
+// };
+// leftBTN.addEventListener("click", backslide);
 
 // The what I do function
-const header1 = document.querySelector(".header-1");
-const firstDiv = document.querySelector(".what-I-do");
+// const header1 = document.querySelector(".header-1");
+// const firstDiv = document.querySelector(".what-I-do");
 
-whatIdo = () => {
-  fetch(`./projectDesc.json`)
-    .then((res) => res.json())
-    .then((data) => {
-      document.querySelector(".header-1").style.display = "none";
-      document.getElementById("hidden").innerHTML = `<ol>
-      <li>'${data[1].WhatIdo}'</li>
-      <li>'${data[2].listone}'</li>
-      <li>'${data[3].listtwo}'</li></ol>`;
-    });
-};
-header1.addEventListener("mouseover", whatIdo);
+// whatIdo = () => {
+//   fetch(`./projectDesc.json`)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       document.querySelector(".header-1").style.display = "none";
+//       document.getElementById("hidden").innerHTML = `<ol>
+//       <li>'${data[1].WhatIdo}'</li>
+//       <li>'${data[2].listone}'</li>
+//       <li>'${data[3].listtwo}'</li></ol>`;
+//     });
+// };
+// header1.addEventListener("mouseover", whatIdo);
 
-const buttonLeft = document.querySelector(".left");
-const buttonRight = document.querySelector(".right");
-const projectOne = document.querySelector(".div1");
-const projectTwo = document.querySelector(".div2");
+// const buttonLeft = document.querySelector(".left");
+// const buttonRight = document.querySelector(".right");
+// const projectOne = document.querySelector(".div1");
+// const projectTwo = document.querySelector(".div2");
 
-slideproject = () => {
-  projectTwo.classList.remove("hidden-div");
-  projectOne.classList.add("hidden-div");
-};
-buttonRight.addEventListener("click", slideproject);
+// slideproject = () => {
+//   projectTwo.classList.remove("hidden-div");
+//   projectOne.classList.add("hidden-div");
+// };
+// buttonRight.addEventListener("click", slideproject);
