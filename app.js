81e471f1923251f8b2df0850e92b1186bf0b8af8
priorityc <petya83dimitrova@gmail.com>
2023-02-06 19:24:15 +0000

    Form-Validation fixed

--- app.js
+++ app.js
@@ -40,10 +40,10 @@ const lastnameEl = document.querySelector("#last-name");
 const emailEl = document.querySelector("#email");
 const messageEl = document.querySelector("#message");
 
-const form = document.querySelector("#form");
+const formEl = document.getElementById("#form");
 
 // attach submit event to the form
-form.addEventListener("submit", function (e) {
+formEl.addEventListener("submit", function (e) {
   // prevent the form from submitting
   e.preventDefault();
 });
@@ -168,15 +168,8 @@ const chackMessage = () => {
   return valid;
 };
 
-const sendEmail = () => {
-  form.setAttribute("action", "mailto:petya83dimitrova@gmail.com");
-};
-
 // Modify the submit event handler
-const validateModal = (e) => {
-  // prevent the form from submitting
-  e.preventDefault();
-
+const validateModal = () => {
   // validate forms
   let isnameValid = checkUsername(),
     islastnameValid = chacklastName(),
@@ -189,21 +182,153 @@ const validateModal = (e) => {
   // submit to the server if the form is valid
   // Finally, submit data to the server if the form is valid specified the isFormValid flag.
   if (isFormValid) {
-    sendEmail();
+    formEl.submit();
   }
 };
-form.addEventListener("submit", validateModal);
+formEl.addEventListener("submit", validateModal);
 
 // validate the footer form
-const nameData = document.getElementById("contact-name").value;
-const emailData = document.getElementById("contact-email").value;
-const messageData = document.getElementById("contact-message").value;
-console.log(nameData);
+// elements
+const nameInput = document.getElementById("contact-name");
+const emailInput = document.getElementById("contact-email");
+const messageInput = document.getElementById("contact-message");
+const submitBTN = document.getElementById("save");
+// the form
+const foterForm = document.getElementById("form-footer");
 
-validateFooterForm = (e) => {
+/// attach submit event to the form
+foterForm.addEventListener("submit", function (e) {
+  // prevent the form from submitting
   e.preventDefault();
+});
+// The following isRequired() function returns true if the input argument is empty:
+const isRequired2 = (value) => (value === "" ? false : true);
+// The following isBetween() function returns false if the length argument is not between the min and max argument:
+const isBetween2 = (length, min, max) =>
+  length < min || length > max ? false : true;
+// check if the email is valid with regular expresion
+const isEmailValid2 = (email) => {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+};
 
-  if (nameData === "") {
-    return alert("Please write your name!");
+// The function that showError() function highlights the border of the input field and displays an error message if the input field is invalid:
+const showError2 = (input, message) => {
+  // get the perent element of the input field which is the div el
+  const formField2 = input.parentElement;
+  //Second, remove the success class and add the error class to the form-field element:
+  formField2.classList.remove("success");
+  formField2.classList.add("error");
+
+  // show the error message
+  const error = formField2.querySelector("small");
+  error.textContent = message;
+};
+
+//Same function for Sucess
+const showSuccess2 = (input) => {
+  // get the form-field element
+  const formField2 = input.parentElement;
+
+  // remove the error class
+  formField2.classList.remove("error");
+  formField2.classList.add("success");
+
+  // hide the error message
+  const error = formField2.querySelector("small");
+  error.textContent = "";
+};
+
+const checkUsername2 = () => {
+  let valid = false;
+  const min = 3,
+    max = 25;
+  const nameE = nameInput.value.trim();
+
+  if (!isRequired2(nameE)) {
+    showError(nameInput, "Username cannot be blank.");
+  } else if (!isBetween(nameE.length, min, max)) {
+    showError(nameInput, `Name must be between ${min} and ${max} characters.`);
+  } else {
+    showSuccess2(nameInput);
+    valid = true;
+  }
+  // return true if passes
+  return valid;
+};
+
+const checkEmail2 = () => {
+  // initial default state
+  let valid = false;
+  const email = emailInput.value.trim();
+  if (!isRequired2(email)) {
+    showError2(emailInput, "Email cannot be blank.");
+  } else if (!isEmailValid(email)) {
+    showError2(emailInput, "Email is not valid.");
+  } else {
+    showSuccess2(emailInput);
+    valid = true;
+  }
+  return valid;
+};
+
+const chackMessage2 = () => {
+  let valid = false;
+  const min = 3,
+    max = 40;
+  const messageE = messageInput.value.trim();
+  if (!isRequired2(messageE)) {
+    showError2(messageInput, "Message cannot be blank.");
+  } else if (!isBetween2(messageE.length, min, max)) {
+    showError2(
+      messageEl,
+      `Message must be between ${min} and ${max} characters.`
+    );
+  } else {
+    showSuccess2(messageInput);
+    valid = true;
+  }
+  // return true if passes
+  return valid;
+};
+
+// Modify the submit event handler
+const validateFooter = () => {
+  // validate forms
+  let isnameValid = checkUsername2(),
+    isEmailValid = checkEmail2(),
+    isMessageValid = chackMessage2();
+
+  let isFormValid = isnameValid && isEmailValid && isMessageValid;
+
+  // submit to the server if the form is valid
+  // Finally, submit data to the server if the form is valid specified the isFormValid flag.
+  if (isFormValid) {
+    foterForm.submit();
   }
 };
+submitBTN.addEventListener("submit", validateFooter);
+//   // return true if passes
+//   return valid;
+// };
+
+// const validateFooterForm = () => {
+//   // prevent the form from submitting
+
+//   // validate forms
+//   let isnameValid = checkName(),
+//     isEmailValid = checkEmaill(),
+//     isMessageValid = chackMessages();
+
+//   let isFooterFormValid = isnameValid && isEmailValid && isMessageValid;
+
+//   // submit to the server if the form is valid
+//   // Finally, submit data to the server if the form is valid specified the isFormValid flag.
+//   if (isFooterFormValid) {
+//     foterForm.submit();
+//   }
+// };
+// foterForm.addEventListener("submit", validateFooterForm);
+
+//declare two patterns
